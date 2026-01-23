2474e5781f7ac4bfdf45054c0222579c5995f2fe
KimDuHyun-web <duhyeon467@gmail.com>
2026-01-23 16:40:20 +0900

    feat: insert 페이지

--- src/app/insert/page.js
+++ src/app/insert/page.js
@@ -1,9 +1,23 @@
+"use client"
+import { createClient } from "@/utils/supabase/client";
+
 export default function Insert() {
+  const supabase = createClient();
+
+  const onSubmit = async (e)=>{
+    e.preventDefault();
+    const { error } = await supabase
+    .from('portfolio')
+    .insert({ title:'제목 테스트', content: '본문 테스트'})
+  
+    console.log(error);
+  }
+
   return (
     <>
       <h2 className="text-center">프로젝트 입력</h2>
       <div className="contact_form">
-        <form action="">
+        <form action="" onSubmit={onSubmit}>
           <p className="field">
             <label htmlFor="title">프로젝트 제목:</label>
             <input type="text" id="title" placeholder="프로젝트 제목" />
